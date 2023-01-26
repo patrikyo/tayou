212fc8d2815f989476240e69aafcb877fa0e055c
patrik <patrik.youssef@arbetsformedlingen.se>
2023-01-26 19:45:08 +0100

    Adds hero component

---
+++ src/hero/Hero.js
@@ -0,0 +1,19 @@
+import "./Hero.css";
+function Hero() {
+  return (
+    <div class="hero-container">
+      <div class="container">
+        <div class="hero-text-container">
+          <h2 class="hero-header">Lorum ipsum</h2>
+          <p class="hero-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            feugiat gravida sem et dapibus. Ut in consequat nisi, at fermentum
+            dui. Vestibulum a eros ut mauris fermentum dignissim nec eu nisl
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Header from "./header/Header";
+import Hero from "./hero/Hero";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Header />
+    <Hero />
     <App />
   </React.StrictMode>
 );
