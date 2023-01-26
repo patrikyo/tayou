13daba2c81f3265e38fa18a6607d8b0218551103
patrik <patrik.youssef@arbetsformedlingen.se>
2023-01-26 19:45:42 +0100

    Improves styling and name convention for component

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,13 +1,13 @@
 import "./Header.css";
 function Header() {
   return (
-    <div class="container pt-3">
+    <div id="header-container" class="container pt-3">
       <div class="row">
         <div class="col-10">
           <h1>TAYOU</h1>
         </div>
         <nav class="col-2">
-          <ul class="navigation-links">
+          <ul class="header-navigation-links">
             <li>
               <a href="www.google.se">Hem</a>
             </li>
