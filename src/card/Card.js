dfcbecd89a7c7ff01e2ed0534577ae5e1d73b71b
patrik <patrik.youssef@arbetsformedlingen.se>
2023-01-28 08:43:38 +0100

    Adds cards and fontAwsome icons

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,5 +1,24 @@
+import "./Card.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsersLine } from "@fortawesome/free-solid-svg-icons";
+
 const Card = () => {
-  return <div></div>;
+  return (
+    <div className="card">
+      <div class="image">
+        <FontAwesomeIcon icon={faUsersLine} />
+      </div>
+      <div className="card-body">
+        <h2 className="card-title">Special title treatment</h2>
+        <p className="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <a href="www.google.se" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/section/Section.js
+++ src/section/Section.js
@@ -1,5 +1,23 @@
-const Section = () => {
-  return <div></div>;
+import Card from "../card/Card";
+import "./Section.css";
+const Section = (props) => {
+  return (
+    <div class="section__container container">
+      <div class="section__header">
+        {props.headingLevel === 2 ? (
+          <h2>{props.header}</h2>
+        ) : (
+          <h3>{props.header}</h3>
+        )}
+      </div>
+      <div class="section__body">{props.body}</div>
+      <div class="section__card">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 };
 
 export default Section;
