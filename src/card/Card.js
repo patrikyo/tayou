2150687dff1015dfb60f1720d6f30e5124cfc7c0
patrik <patrik.youssef@arbetsformedlingen.se>
2023-02-19 19:31:27 +0100

    Adds card component

--- src/card/Card.js
+++ src/card/Card.js
@@ -4,16 +4,33 @@ import { faUsersLine } from "@fortawesome/free-solid-svg-icons";
 
 const Card = (props) => {
   return (
-    <div className="card__container">
-      <div class="card__icon-container">
-        <FontAwesomeIcon icon={faUsersLine} />
-      </div>
+    <div
+      className={`${
+        props.theme === "light" ? "card__container" : "card__container--dark"
+      }`}
+    >
+      {props.icon.length > 0 && (
+        <div class="card__icon-container">
+          {props.icon} faUsersLine
+          <FontAwesomeIcon icon="faUsersLine" />
+        </div>
+      )}
+      {props.imgUrl.length > 0 && (
+        <img className="card__image" src={props.imgUrl} alt="" />
+      )}
       <div className="card__body">
         <h2 className="card__title">{props.title}</h2>
         <p className="card__text">{props.description}</p>
-        <a href="www.google.se" className="btn btn-primary">
-          Mer information
-        </a>
+        {props.showButton && (
+          <a
+            href="www.google.se"
+            className={`${
+              props.theme === "light" ? "btn btn-primary" : "card__button--dark"
+            }  `}
+          >
+            {`Läs mer om ${props.title}`}
+          </a>
+        )}
       </div>
     </div>
   );
